3e4817fd2d1c1e686c0b589872afd845bd2d7dd9
Bartosz Gutowski <bartosz.gutowski@student.put.poznan.pl>
2016-05-15 19:00:52 +0200

    3 testy

--- src/main/java/HttpsGetter.java
+++ src/main/java/HttpsGetter.java
@@ -1,14 +1,8 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import utils.StringUtils;
 
 import javax.net.ssl.HttpsURLConnection;
-
-import utils.StringUtils;
+import java.io.IOException;
+import java.net.URL;
 /**
  * Klasa pobierająca dokument HTTP z podanego adresu.
  * @author Tomek
