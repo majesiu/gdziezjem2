0dc73720bb58287f3e50b62417dcde7fd9802f5a
Tomek <tomaszbieniek@vp.pl>
2016-05-15 19:33:39 +0200

    JavaDoc do 7 klas.

--- src/main/java/modelfactories/RestaurantFactory.java
+++ src/main/java/modelfactories/RestaurantFactory.java
@@ -3,9 +3,21 @@ package modelfactories;
 import models.RestaurantModel;
 import org.json.JSONException;
 import org.json.JSONObject;
-
+/**
+ * Klasa tworząca obiekty typu RestaurantModel, korzystająca z wzorca projektowego fabryki.
+ * @author Tomek
+ * @see RestaurantModel
+ * @see ModelFactory
+ */
 public class RestaurantFactory extends ModelFactory<RestaurantModel> {
-
+	/**
+	 * Tworzy obiekt typu RestaurantModel korzystając z obiektu typu JSONObject
+	 * 
+	 * @param o
+	 * 			Obiekt JSON
+	 * @return 
+	 * 			Obiekt typu RestaurantModel
+	 */
 	@Override
 	public RestaurantModel createModelFromJSONObject(JSONObject o) throws JSONException {
 		int id = o.getInt("id");
