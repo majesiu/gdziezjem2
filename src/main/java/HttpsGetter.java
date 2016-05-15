b304cd0179bc15d0e6cd6ebb5266eab27bf482d2
Bartosz Gutowski <bartosz.gutowski@student.put.poznan.pl>
2016-05-15 18:59:50 +0200

    3 testy

--- src/main/java/HttpsGetter.java
+++ src/main/java/HttpsGetter.java
@@ -24,11 +24,11 @@ public class HttpsGetter {
 	 * 			Wyjątek wyrzucany jeżeli podczas próby połączenia ze stroną o padanym adresie otrzymamy kod zwrotny inny niż OK (połączenie nie powiodło się) 
 	 */
 	public static String HttpsGet(String urlStr) throws IOException {
-		URL url = new URL(urlStr);
+		URL url = new URL(urlStr);/
 		HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
 		return HttpsGet_inside(connection);
 	}
-	private static String HttpsGet_inside(HttpsURLConnection connection) throws IOException{
+	private static String HttpsGet_inside(HttpsURLConnection connection) throws IOException{ // do mocka
 		connection.setRequestMethod("GET");
 
 		if(connection.getResponseCode() != 200){
